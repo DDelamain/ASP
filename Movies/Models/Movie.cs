abcb16abd7990de70ea44b2f3cb3bdb3a63a9069
dando <danila.dor.1125@mail.ru>
2026-09-12 23:06:04 +0500

    Lesson 20 - DataAnnotations

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -1,10 +1,22 @@
-﻿namespace Movies.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
 {
 	public class Movie
 	{
 		public int Id { get; set; }
+
+		[Required]
+		//[StringLength(50, MinimumLength = 2)]
+		[MinLength(2, ErrorMessage = "Слишком мало"), MaxLength(50, ErrorMessage = "Слишком много")]
+		[Display(Name = "Название")]
 		public string Title { get; set; }
+
+		[RangeAttribute(typeof(DateOnly), "1888-10-14","2128-12-31", ErrorMessage = "Тогда кино еще не снимали")]
+		[Display(Name = "Дата выхода")]
 		public DateOnly ReleaseDate { get; set; }
+
+		[Display(Name = "Жанр")]
 		public string? Genre { get; set; }
 		public string? URL {  get; set; }
 		public string Poster {  get; set; }
