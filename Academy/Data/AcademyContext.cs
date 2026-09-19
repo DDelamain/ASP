685e8b3264a02d230bcb835043754f48c7b0556c
dando <danila.dor.1125@mail.ru>
2026-09-19 23:39:22 +0500

    Lesson 26 - Groups in progress

--- Academy/Data/AcademyContext.cs
+++ Academy/Data/AcademyContext.cs
@@ -15,5 +15,6 @@ namespace Academy.Data
 		}
 
 		public DbSet<Academy.Models.Direction> Directions { get; set; } = default!;
+	    public DbSet<Academy.Models.Group> Group { get; set; } = default!;
 	}
 }
